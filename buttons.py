b79047a8e8bb07929128506542aa2054f370c44f
Timbusien <timbusiennavi@gmail.com>
2023-09-09 22:00:01 +0500

    new

--- buttons.py
+++ buttons.py
@@ -19,3 +19,12 @@ def geo():
     geo_button = types.KeyboardButton('send location', request_location=True)
     buttons.add(geo_button)
     return buttons
+
+def inline():
+    buttons = types.InlineKeyboardMarkup()
+    in_b = types.InlineKeyboardButton('Inline1', callback_data = 'in_button')
+    in_b2 = types.InlineKeyboardButton('Inline2', callback_data = 'in_button2')
+    buttons.row(in_b, in_b2)
+    return buttons
+
+
